7d52d85650dff7aa9cd83a91cc8c53b5f251fefa
GitHub99999 <toni.stoinev@gmail.com>
2019-07-11 23:00:48 +0300

    Added CreditCardController.cs and CreditCard Details view

---
+++ VinetkiBG/VinetkiBG/Controllers/CreditCardController.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace VinetkiBG.Controllers
+{
+    public class CreditCardController : Controller
+    {
+        public IActionResult Details()
+        {
+            return View();
+        }
+    }
+}
