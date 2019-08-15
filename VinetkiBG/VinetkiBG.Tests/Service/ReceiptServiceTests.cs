d2a7c81aa722e40cc18ae6db95eed32a0932a306
GitHub99999 <toni.stoinev@gmail.com>
2019-08-15 22:43:21 +0300

    Added RecipietService test methods

--- VinetkiBG/VinetkiBG.Tests/Service/ReceiptServiceTests.cs
+++ VinetkiBG/VinetkiBG.Tests/Service/ReceiptServiceTests.cs
@@ -1,10 +1,79 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Linq;
 using System.Text;
+using VinetkiBG.Domain;
+using VinetkiBG.Models.ServiceModels;
+using VinetkiBG.Services;
+using VinetkiBG.Tests.Common;
+using Xunit;
 
 namespace VinetkiBG.Tests.Service
 {
     public class ReceiptServiceTests
     {
+        private IReceiptService receiptService;
+
+        public ReceiptServiceTests()
+        {
+            MapperInitializer.InitializeMapper();
+        }
+
+        [Fact]
+        public void CreateReceipt_WithCorrectData_ShouldWorkProperly()
+        {
+            var context = VinetkiBGDbContextInMemoryFactory.InitializeContext();
+            this.receiptService = new ReceiptService(context);
+
+            ReceiptServiceModel receiptServiceModel = new ReceiptServiceModel
+            {
+                LicensePlate = "fsdjfd",
+                StartDate = DateTime.UtcNow,
+                EndDate = DateTime.UtcNow.AddDays(30),
+                Price = 30,
+                VehicleType = "Bus",
+                VignetteId = "fsfs"
+            };
+
+            string actualResult = this.receiptService.CreateReceipt(receiptServiceModel);
+
+            Assert.True(actualResult != null, "CreateRecipiet() doesn't work properly !");
+        }
+
+        [Fact]
+        public void GetReceiptById_WithCorrectData_ShouldWorkProperly()
+        {
+            var context = VinetkiBGDbContextInMemoryFactory.InitializeContext();
+            this.receiptService = new ReceiptService(context);
+
+            #region Dummy Data
+            context.Receipts.Add(new Receipt
+            {
+                Id = "abcd",
+                LicensePlate = "KH4934BA",
+                StartDate = DateTime.UtcNow,
+                EndDate = DateTime.UtcNow.AddDays(30),
+                Price = 15,
+                VignetteId = "fsdfs",
+                VehicleType = "Bus"
+            });
+
+            context.SaveChanges();
+            #endregion
+
+            var receiptFromDb = context.Receipts.First();
+
+            var exectedResult = AutoMapper.Mapper.Map<ReceiptServiceModel>(receiptFromDb);
+
+            var actualResult = this.receiptService.GetReceiptById("abcd");
+
+            Assert.True(exectedResult.Id == actualResult.Id, "Id is not returned properly !");
+            Assert.True(exectedResult.LicensePlate == actualResult.LicensePlate, "License Plate is not returned properly !");
+            Assert.True(exectedResult.Price == actualResult.Price, "Price is not returned properly !");
+            Assert.True(exectedResult.StartDate == actualResult.StartDate, "Start Date is not returned properly !");
+            Assert.True(exectedResult.EndDate == actualResult.EndDate, "End date is not returned properly !");
+            Assert.True(exectedResult.VignetteId == actualResult.VignetteId, "Vignette Id is not returned properly !");
+            Assert.True(exectedResult.VehicleType == actualResult.VehicleType, "Vehicle Type is not returned properly !");
+        }
     }
 }
