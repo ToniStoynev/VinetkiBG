320daaab8b5a7dd37322df7966ac71ce400b288d
GitHub99999 <toni.stoinev@gmail.com>
2019-08-29 11:31:32 +0300

    Added validation for RegisterCreditCard

--- VinetkiBG/VinetkiBG/Models/BidingModels/RegisterCreditCarBindingModel.cs
+++ VinetkiBG/VinetkiBG/Models/BidingModels/RegisterCreditCarBindingModel.cs
@@ -8,18 +8,26 @@ namespace VinetkiBG.Models.BidingModels
 {
     public class RegisterCreditCarBindingModel
     {
+        private const string BankErrorMessage = "Bank Name should be between 3 and 12 symbols";
+        private const string IBANErrorMessage = "IBAN should be exactly 10 characters";
+        private const string BICErrorMessage = "BIC should be between 4 and 9 characters";
+        private const string AmountErrorMessage = "Amount should be between 10 and 1 000 000";
         public string Id { get; set; }
 
         [Required]
+        [StringLength(maximumLength: 12, ErrorMessage = BankErrorMessage, MinimumLength =3)]
         public string BankName { get; set; }
 
         [Required]
+        [StringLength(maximumLength: 10, ErrorMessage = IBANErrorMessage, MinimumLength = 10)]
         public string IBAN { get; set; }
 
         [Required]
+        [StringLength(maximumLength: 9, ErrorMessage = BICErrorMessage, MinimumLength = 4)]
         public string BIC { get; set; }
 
         [Required]
+        [Range(10, 1000000, ErrorMessage = AmountErrorMessage)]
         public decimal TotalAmount { get; set; }
 
     }
